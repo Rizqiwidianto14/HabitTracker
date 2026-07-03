d7d2120448c9ee3e370eaaeac49b0b60505e32cb
Rizqi Widianto <widiantorizqi@gmail.com>
2026-07-03 11:17:38 +0700

    feature habit 1st commit

--- HabitTracker/Features/MV/Views/MVEmptyStateView.swift
+++ HabitTracker/Features/MV/Views/MVEmptyStateView.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct MVEmptyStateView: View {
     var body: some View {
         ContentUnavailableView(
-            "No Habits Yet",
+            "No Habits Yet Bro",
             systemImage: "checkmark.circle",
             description: Text("Tap + to add your first habit and start tracking your progress.")
         )
